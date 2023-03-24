b3007d592f09c5403e49fc5d95a9ffb776c5aae2
Marcin Smarzewski <marcin.smarzewski@thefarm51.com>
2023-03-24 01:13:02 +0100

    Add fonts

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,12 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-			aspectRatio: {
-				card: "1 / 1.91",
-			},
-		},
+      fontFamily: {
+        sans: ["Libre Franklin", ...defaultTheme.fontFamily.sans],
+      },
+      aspectRatio: {
+        card: "1 / 1.91",
+      },
+    },
   },
   plugins: [],
 };
