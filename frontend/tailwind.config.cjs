c9e1eb8c0c0aa504886cc67727253c31834ff593
Marcin Smarzewski <marcin.smarzewski@thefarm51.com>
2023-03-25 20:12:18 +0100

    Finish styling config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,15 +1,17 @@
+const plugin = require("tailwindcss/plugin");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ["hidden"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Libre Franklin", ...defaultTheme.fontFamily.sans],
       },
       aspectRatio: {
-        card: "1 / 1.91",
+        card: "2 / 3",
       },
     },
   },
