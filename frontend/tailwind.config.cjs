b164fe1e3e6b58713754ce8fe64a4dfb6d2cd9ba
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-04 01:02:51 +0200

    add animations

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,16 +1,19 @@
 const plugin = require("tailwindcss/plugin");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+const durationValuesMs = [0, 75, 100, 150, 200, 300, 500, 700, 1000];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   safelist: ["hidden"],
   theme: {
     extend: {
-      animation: {
-        appear: "appear .5s ease-out both",
-        appearReversed: "appearReversed .5s ease-out both",
-      },
+      animation: durationValuesMs.reduce((acc, ms) => ({
+        ...acc,
+        [`appear-${ms}`]: `appear ${ms}ms ease-out both`,
+        [`appear-reversed-${ms}`]: `appearReversed ${ms}ms ease-out both`,
+      })),
       keyframes: {
         appear: {
           "0%": { transform: `scale(0.1)`, opacity: 0 },
