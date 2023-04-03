77c644d227e8a402f18a0dc85e24b929e3b29390
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-03 21:10:52 +0200

    Add animations

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,6 +7,20 @@ module.exports = {
   safelist: ["hidden"],
   theme: {
     extend: {
+      animation: {
+        appear: "appear .5s ease-out both",
+        appearReversed: "appearReversed .5s ease-out both",
+      },
+      keyframes: {
+        appear: {
+          "0%": { transform: `scale(0.1)`, opacity: 0 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        },
+        appearReversed: {
+          "0%": { transform: `scale(${1.5})` },
+          "100%": { transform: "scale(1)" },
+        },
+      },
       fontFamily: {
         sans: ["Libre Franklin", ...defaultTheme.fontFamily.sans],
       },
