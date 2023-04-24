2db9461fd1eff91498f9d597ceb87ad73da7b21f
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-24 23:01:21 +0200

    Better animations

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -8,18 +8,18 @@ module.exports = {
   safelist: ["hidden"],
   theme: {
     extend: {
-			colors:{
-				"primary-800": "#635A4C",
-				"primary-700": "#7C705F",
-				"primary-500": "#F1C47E",
-				"primary-300": "#FFDFAF",
-				"primary-200": "#ECE3C6",
-				"secondary-800": "#765F56",
-				"secondary-500": "#E48E67",
-				"secondary-400": "#FABA9E",
-				"secondary-300": "#FFC8AF",
-				"secondary-100": "#FFFBF5",
-			},
+      colors: {
+        "primary-800": "#635A4C",
+        "primary-700": "#7C705F",
+        "primary-500": "#F1C47E",
+        "primary-300": "#FFDFAF",
+        "primary-200": "#ECE3C6",
+        "secondary-800": "#765F56",
+        "secondary-500": "#E48E67",
+        "secondary-400": "#FABA9E",
+        "secondary-300": "#FFC8AF",
+        "secondary-100": "#FFFBF5",
+      },
       animation: durationValuesMs.reduce((acc, ms) => ({
         ...acc,
         [`appear-${ms}`]: `appear ${ms}ms ease-out both`,
@@ -28,6 +28,7 @@ module.exports = {
       keyframes: {
         appear: {
           "0%": { transform: `scale(0.1)`, opacity: 0 },
+          "30%": { opacity: 0 },
           "100%": { transform: "scale(1)", opacity: 1 },
         },
         appearReversed: {
