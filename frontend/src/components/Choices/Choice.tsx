86cf4c93ae1f0167ee48003e46d4eadf1ea53964
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-24 22:08:50 +0200

    Make it more interactive, add revert button

--- frontend/src/components/Choices/Choice.tsx
+++ frontend/src/components/Choices/Choice.tsx
@@ -1,6 +1,7 @@
 import mainPageStore from "../../stores/mainPage/mainPage";
 import classNames from "classnames";
 import type { Choice } from "../../stores/mainPage/slices/customization";
+import classes from "./Choices.module.scss";
 
 interface Props {
   choiceData: Choice | undefined;
@@ -21,9 +22,13 @@ const Choice = ({ choiceData, index }: Props) => {
       style={{ "background-image": choiceData?.backgroundImage }}
       tabIndex={choiceData ? 0 : -1}
       aria-hidden={!choiceData}
+      aria-label={choiceData?.title || ""}
       class={classNames(
-        "radius rounded-full aspect-square w-16 animate-appear-300 bg-stone-100 bg-cover bg-center",
-        !choiceData?.backgroundImage && "pointer-events-none"
+        "radius rounded-full aspect-square w-16 animate-appear-300 bg-white bg-cover bg-center",
+        choiceData &&
+          "after:rounded-xl after:p-2 after:whitespace-nowrap after:bg-stone-700 after:text-white",
+        !choiceData?.backgroundImage && "pointer-events-none",
+        choiceData && classes.choice
       )}
     />
   );
