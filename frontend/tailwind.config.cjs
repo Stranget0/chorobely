0a1842c031cf5e5b03502580381acd61899cba41
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-25 01:15:42 +0200

    Price tag animation

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,10 +2,19 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 const durationValuesMs = [0, 75, 100, 150, 200, 300, 500, 700, 1000];
 
+const animations = durationValuesMs.reduce((acc, ms) => ({
+  ...acc,
+  [`appear-${ms}`]: `appear ${ms}ms ease-out both`,
+  [`appear-reversed-${ms}`]: `appearReversed ${ms}ms ease-out both`,
+  [`hide-down-${ms}`]: `hideDown ${ms}ms ease-out both`,
+  [`show-down-${ms}`]: `showDown ${ms}ms ease-out both`,
+}));
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  safelist: ["hidden"],
+  safelist: ["hidden", "animate-show-down-200", "animate-hide-down-200"],
   theme: {
     extend: {
       colors: {
@@ -20,11 +29,7 @@ module.exports = {
         "secondary-300": "#FFC8AF",
         "secondary-100": "#FFFBF5",
       },
-      animation: durationValuesMs.reduce((acc, ms) => ({
-        ...acc,
-        [`appear-${ms}`]: `appear ${ms}ms ease-out both`,
-        [`appear-reversed-${ms}`]: `appearReversed ${ms}ms ease-out both`,
-      })),
+      animation: animations,
       keyframes: {
         appear: {
           "0%": { transform: `scale(0.1)`, opacity: 0 },
@@ -34,6 +39,14 @@ module.exports = {
         appearReversed: {
           "0%": { transform: `scale(${1.5})` },
           "100%": { transform: "scale(1)" },
+        },
+        hideDown: {
+          "0%": { transform: `translateX(-50%) translateY(0%)`, opacity: 1 },
+          "100%": { transform: `translateX(-50%) translateY(100%)`, opacity: 0 },
+        },
+        showDown: {
+          "0%": { transform: `translateX(-50%) translateY(-100%)`, opacity: 0 },
+          "100%": { transform: `translateX(-50%) translateY(0)`, opacity: 1 },
         },
       },
       fontFamily: {
